d820e392ab88c1ccd784ccfbec766399db396f37
Rajguru09 <rajesh09guru@gmail.com>
2025-05-07 16:40:03 +0530

    Update db.py

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -3,3 +3,10 @@ from app.core.config import settings
 
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(settings.DYNAMODB_TABLE)
+
+# Test to ensure we can access the table
+try:
+    response = table.describe_table()
+    print(f"Table {settings.DYNAMODB_TABLE} exists!")
+except Exception as e:
+    print(f"Error accessing table: {e}")
