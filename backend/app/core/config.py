44bdbc6c31348ae87511ae4523c17485f25ad36c
Rajguru09 <rajesh09guru@gmail.com>
2025-05-07 16:37:44 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,15 @@
+#backend/app/core/config.py
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
 
 class Settings:
-    PROJECT_NAME = "CleanCloud"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
-    JWT_ALGORITHM = "HS256"
-    DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE", "cleancloud_users")
+    PROJECT_NAME: str = "CleanCloud"
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY", "super-secret")  # Secret key for JWT
+    JWT_ALGORITHM: str = "HS256"  # JWT algorithm used for encoding/decoding tokens
+    DYNAMODB_TABLE: str = os.getenv("DYNAMODB_TABLE", "users")  # DynamoDB table name
 
+# Create an instance of Settings to access configuration values
 settings = Settings()
