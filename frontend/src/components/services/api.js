54019a074607ff0e4934fddc7e9238d005b5b8b7
Rajguru09 <rajesh09guru@gmail.com>
2025-05-08 16:35:45 +0530

    Update api.js

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,6 +1,6 @@
 // frontend/src/components/services/api.js
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"; // Set via .env file
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Centralized response handler
 const handleResponse = async (res) => {
