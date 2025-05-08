2b84bf3182ce33d795972ab6cde3b5f5eafc7556
Rajguru09 <rajesh09guru@gmail.com>
2025-05-08 17:30:30 +0530

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,10 @@
+//#backend/app/models/user.py
 from pydantic import BaseModel, EmailStr, Field, model_validator
+from enum import Enum
+
+class RoleEnum(str, Enum):
+    basic = "basic"
+    admin = "admin"  # You can add more roles as needed
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -10,7 +16,7 @@ class UserCreate(BaseModel):
         if self.password != self.confirm_password:
             raise ValueError("Passwords do not match")
         return self
-        
+
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
@@ -18,7 +24,7 @@ class UserLogin(BaseModel):
 class UserOut(BaseModel):
     uid: str
     email: EmailStr
-    role: str = "basic"  # Default to basic role, can be modified later if needed.
+    role: RoleEnum = RoleEnum.basic  # Default to basic role
 
     class Config:
         # Ensure that model output is in snake_case format
